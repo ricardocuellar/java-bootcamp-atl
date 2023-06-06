3f183164e928d2fb703a2179d3fb0c523d13330e
Ricardo Cuellar <rcuellarsn@gmail.com>
2023-06-06 15:44:04 -0600

    Add: Actividad 4 Calculadora de propinas en clase_5

--- clase_5/CalculadoraPropinas.java
+++ clase_5/CalculadoraPropinas.java
@@ -1,2 +1,26 @@
-package clase_5;public class CalculadoraPropinas {
+package clase_5;
+
+import java.util.Scanner;
+
+public class CalculadoraPropinas {
+    public static void main(String args[]){
+        Scanner scanData = new Scanner(System.in);
+
+        System.out.println("Ingresa el total de la cuenta");
+        double cuenta = scanData.nextDouble();
+
+        System.out.println("¿Qué propina desea dejar?");
+        double porcentajePropina = scanData.nextInt();
+
+        if(cuenta>0 && porcentajePropina > 0){
+            double propina = cuenta * (porcentajePropina / 100);
+            System.out.print("El total a pagar es: " + propina);
+        }
+
+
+
+
+
+
+    }
 }
