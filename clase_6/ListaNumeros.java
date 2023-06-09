4a367566db6d16567362e8099436dd5497e5deb0
Ricardo Cuellar <rcuellarsn@gmail.com>
2023-06-08 23:03:31 -0600

    Add: Actividad 7 lista de numeros del 1 al 100 en clase_6

---
+++ clase_6/ListaNumeros.java
@@ -0,0 +1,13 @@
+package clase_6;
+
+public class ListaNumeros {
+    public static void main(String args[]){
+        System.out.println("Listado de 100 números");
+        int limite = 100;
+        int counter = 0;
+        while(counter < limite){
+            System.out.println(counter+1);
+            counter++;
+        }
+    }
+}
